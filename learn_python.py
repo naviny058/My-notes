adb6ca59edcc28b5ed841ccf65152e9862b3535a
Navin Yadav <114822499+naviny058@users.noreply.github.com>
2024-01-02 12:59:42 +0530

    Add files via upload

---
+++ learn_python.py
@@ -0,0 +1,26 @@
+# print('hello world')
+
+# Python Indentation
+    # Indentation refers to the spaces at the beginning of a code line.
+
+# Python has no command for declaring a variable.
+    # A variable is created the moment you first assign a value to it.
+    # Variables do not need to be declared with any particular type, and can even change type after they have been set.
+# name = 'navin'
+# print(name)
+# name = 23
+# print(name)
+    # casting If you want to specify the data type of a variable, this can be done with casting.
+# name = str(5)
+# print(type(name))
+# print(name)
+# name = 3
+# print(type(name))
+# print(name)
+
+
+# Many Values to Multiple Variables
+# name1, name2, name3 = 'vikas', 'saroj', 'ashish'
+# print(name1)
+# print(name2)
+# print(name3)
